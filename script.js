6afb641dffe3b6888fc42b2f4620a28857fa9533
francasanas1 <103201464+francasanas1@users.noreply.github.com>
2022-06-27 20:55:53 -0300

    Tercer borrador

--- script.js
+++ script.js
@@ -6,48 +6,63 @@ let tiempo = 60;
 
 
 function sumarPuntos() {
-    puntos++;
+    
+    if (puntos === 15) {
+        alert('Ganaste');
+        tiempo=61;
+        puntos=0;
+        return;
+    }
+    moverPunto();
+
+    crearPelotaNegra();
+
+    puntos++;   
+}
+
+function moverPunto(){
+
     document.getElementById('puntos').innerHTML = 'Puntos: ' + puntos;
     numeroRandom = Math.round(Math.random() * 500);
     numeroRandom2 = Math.round(Math.random() * 500);
     document.getElementById('pelota').style.marginTop = numeroRandom + "px";
     document.getElementById('pelota').style.marginLeft = numeroRandom2 + "px";
-    if (puntos === 15){
-        alert('Ganaste');
-        tiempo = 0;
-    }
-    const pelotaNegra = document.createElement('div');
-    
-    for ( puntos=0; puntos<=15; puntos++){
-        
-        pelotaNegra.setAttribute("class","pelotanegra");
-        numeroRandom3 = Math.round(Math.random() * 500);
-        numeroRandom4 = Math.round(Math.random() * 500);
-        pelotaNegra.style.marginTop = numeroRandom3 + "px";
-        pelotaNegra.style.marginLeft = numeroRandom4 + "px";
-        contenedor.append(pelotaNegra);
-        pelotaNegra.addEventListener('mouseover', perder);
-    }
 }
+
 
 let contenedor = document.querySelector('.contenedor');
 
 
-function perder(){
+function perder() {
+    puntos = 0;
     alert('Perdiste');
-    puntos=0;
+    tiempo = 61;
+    return;
 }
 
 pelota.addEventListener('mouseover', sumarPuntos);
 
-function restarTiempo(){
+function restarTiempo() {
     tiempo--;
     document.getElementById('tiempo').innerHTML = 'Tiempo: ' + tiempo;
-    if (tiempo === 0){
+    if (tiempo === 0) {
         alert('Perdiste');
-        tiempo = 0;
-        puntos = 0;
+        tiempo = 61;
     }
 }
-    
+
 setInterval(restarTiempo, 1000);
+
+function crearPelotaNegra() {
+    const pelotaNegra = document.createElement('div');
+    pelotaNegra.setAttribute("class", "pelotanegra");
+
+    numeroRandom3 = Math.round(Math.random() * 500);
+    numeroRandom4 = Math.round(Math.random() * 500);
+
+    pelotaNegra.style.marginTop = numeroRandom3 + "px";
+    pelotaNegra.style.marginLeft = numeroRandom4 + "px";
+
+    contenedor.append(pelotaNegra);
+    pelotaNegra.addEventListener('mouseover', perder);
+}
