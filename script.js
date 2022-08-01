78f324e06506a6b57cc6786f177195de17e81a75
francasanas1 <103201464+francasanas1@users.noreply.github.com>
2022-07-31 22:44:44 -0300

    Proyecto finalizado

--- script.js
+++ script.js
@@ -10,9 +10,15 @@ const nombre = document.querySelector('#nombre');
 const puntuacion = document.querySelector('#puntuacion');
 const jugarDeNuevo = document.getElementById('jugarDeNuevo');
 var pelotaNegra;
+var nombreUsuario;
+var puntuacion1;
+var elementoNombre;
+var elementoPuntacion;
+let puntosGanar = 50;
+var miarray = [];
+var obj;
 
-
-let puntos = 0;
+let puntos = 1;
 let tiempo = 60;
 
 pelota.addEventListener('mouseover', sumarPuntos);
@@ -32,10 +38,12 @@ function perder() {
         title: 'Perdiste...',
         text: 'Que lastima!',
         confirmButtonText: 'Jugar de nuevo'
+    }).then(() => {
+        puntos = puntos - 1;
+        guardarPuntacion();
+        reiniciarJuego();
+        agregarFila();
     })
-    reiniciarJuego();
-    guardarPuntacion();
-    return;
 }
 
 function crearPelotaNegra() {
@@ -59,15 +67,17 @@ function crearPelotaNegra() {
 
 function sumarPuntos() {
 
-    if (puntos === 15) {
+    if (puntos === puntosGanar) {
         Swal.fire({
             icon: 'success',
             title: 'Ganaste!',
             text: 'Felicidades!',
             confirmButtonText: 'Jugar de nuevo'
+        }).then(() => {
+            guardarPuntacion();
+            agregarFila();
+            reiniciarJuego();
         })
-        guardarPuntacion();
-        reiniciarJuego();
     } else {
 
         moverPunto();
@@ -89,105 +99,33 @@ function restarTiempo() {
 function inicioJuego() {
     juego.style.display = "grid";
     registro.style.display = "none";
-    // nombre.innerHTML = `${localStorage.getItem('nombreUsuario')}`;
+    footer.style.display = "none";
     setInterval(restarTiempo, 1000);
-    llenarTablaConDatosGuardados();
 }
 
 function reiniciarJuego() {
     tiempo = 60;
-    puntos = 0;
+    puntos = 1;
     const arrayPN = Array.from(document.getElementsByClassName('pelotanegra'));
     arrayPN.forEach(pelota2 => {
         pelota2.remove(arrayPN);
     })
+    document.getElementById('puntos').innerHTML = 'Puntos: 0';
 }
 
-
-
-// function guardarPuntacion(name, puntuation) {
-//     let nombreUsuario = username.value;
-//     let puntuacion1 = puntuacion.value;
-
-
-//     let obj = {
-//         usuario: name,
-//         puntuacion: puntuation,
-//     };
-
-//     const { usuario, puntuacion } = obj;
-//     //  DESESTRUCTURACIÓN
-
-//     // const miarray = [];
-//     // miarray.push(obj);
-
-//     // miarray.sort((a, b) => {
-//     //     if (a.puntuacion < b.puntuacion) {
-//     //         return 1;
-//     //     }
-//     //     if (a.puntuacion > b.puntuacion) {
-//     //         return -1;
-//     //     }
-//     //     return 0;
-//     // });
-//     // No se si funciona
-
-//     let textoGuardadoP = localStorage.setItem('puntuacion1');
-//     let textoGuardadoN = localStorage.getItem('nombreUsuario');
-
-//     //Si no hay algo almacenado, almaceno por primera vez.
-//     // if (textoGuardado == undefined) {
-//     //     localStorage.setItem('usuarios', JSON.stringify([obj]))
-
-//     //     return
-//     // }
-
-//     if (textoGuardadoN == undefined) {
-
-//         textoGuardadoN = 0;
-
-//         return
-//     }
-
-//     textoGuardado == undefined && localStorage.setItem('usuarios', JSON.stringify(obj));
-//     // OPERADOR AND
-
-//     console.log(typeof textoGuardadoN);
-
-
-//     array = JSON.stringify(textoGuardadoN);
-//     console.log(array, "soy array");
-//     array.push(obj);
-//     console.log(obj);
-//     let json = JSON.stringify(array);
-
-
-//     localStorage.setItem('usuarios', json)
-// }
-
-function llenarTablaConDatosGuardados() {
-    let json = localStorage.getItem('usuarios');
-    let array = JSON.parse(json);
-
-    array.forEach(obj => agregarFila(obj.usuario, obj.puntuation));
-    //  DESESTRUCTURACIÓN
-}
 
 
 registro.addEventListener('submit', (e) => {
     e.preventDefault();
-    let nombreUsuario = username.value;
-    // let puntuacion1 = puntos1.value;
-
+    nombreUsuario = username.value;
     localStorage.setItem('nombreUsuario', nombreUsuario);
-    // localStorage.setItem('puntuacion1', puntuacion1);
     inicioJuego();
 
 })
 
 function guardarPuntacion() {
-    let puntos1 = puntos;
-    let puntuacion1 = puntos1.value;
+    let puntuacion1 = puntos;
+    console.log(puntuacion1);
     let textoGuardadoP = localStorage.setItem('puntuacion1', puntuacion1);
     if (textoGuardadoP == undefined) {
         let puntuacion1 = 0;
@@ -196,7 +134,7 @@ function guardarPuntacion() {
 }
 
 
-function agregarFila(nombre, puntuacion) {
+function agregarFila(nombreUsuario, puntuacion1) {
     let tabla = document.getElementById('tablaPuntuacion');
 
     let tr = document.createElement('tr');
@@ -204,8 +142,11 @@ function agregarFila(nombre, puntuacion) {
     let elementoNombre = document.createElement('td');
     let elementoPuntacion = document.createElement('td');
 
-    elementoNombre.innerText = nombre;
-    elementoPuntacion.innerText = puntuacion;
+    elementoNombre.innerText = localStorage.getItem('nombreUsuario');
+    elementoPuntacion.innerText = localStorage.getItem('puntuacion1');
+
+    // console.log(elementoNombre);
+    // console.log(elementoPuntacion);
 
     tr.appendChild(elementoNombre);
     tr.appendChild(elementoPuntacion);
@@ -213,3 +154,80 @@ function agregarFila(nombre, puntuacion) {
     tabla.appendChild(tr);
 }
 
+function guardarPuntacion1() {
+
+    // var obj = { nombreUsuario, puntuacion1 }
+    //  DESESTRUCTURACIÓN
+
+    var nombreUsuario = username.value;
+    var puntuacion1 = puntos;
+
+    var obj = {
+        usuario: nombreUsuario,
+        puntuacion: puntuacion1,
+    };
+
+    miarray.push(JSON.stringify(obj));
+    console.log(miarray);
+    miarray.push(obj);
+    console.log(obj);
+    let json = JSON.stringify(miarray);
+    localStorage.setItem('usuarios', json)
+
+
+    // miarray.push(obj);
+
+    // console.log(miarray);
+
+    // localStorage.setItem('localArray', JSON.stringify(miarray));
+
+    miarray.sort((a, b) => {
+        if (a.puntuacion1 < b.puntuacion1) {
+            return 1;
+        }
+        if (a.puntuacion1 > b.puntuacion1) {
+            return -1;
+        }
+        return 0;
+    });
+
+
+}
+
+    // let textoGuardadoP = localStorage.setItem('puntuacion1');
+    // let textoGuardadoN = localStorage.getItem('nombreUsuario');
+
+    // //Si no hay algo almacenado, almaceno por primera vez.
+    // // if (textoGuardado == undefined) {
+    // //     localStorage.setItem('usuarios', JSON.stringify([obj]))
+
+    // //     return
+    // // }
+
+    // if (textoGuardadoN == undefined) {
+
+    //     textoGuardadoN = 0;
+
+    //     return
+    // }
+
+    // textoGuardado == undefined && localStorage.setItem('usuarios', JSON.stringify(obj));
+    // // OPERADOR AND
+
+    // console.log(typeof textoGuardadoN);
+
+
+    // array = JSON.stringify(textoGuardadoN);
+    // console.log(array, "soy array");
+    // array.push(obj);
+    // console.log(obj);
+    // let json = JSON.stringify(array);
+    // localStorage.setItem('usuarios', json)
+
+    // function llenarTablaConDatosGuardados() {
+//     let json = localStorage.getItem('usuarios');
+//     let array = JSON.parse(json);
+
+//     array.forEach(obj => agregarFila(obj.usuario, obj.puntuation));
+//     //  DESESTRUCTURACIÓN
+// }
